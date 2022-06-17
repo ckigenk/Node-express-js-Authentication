772fddd2f2640e7c44676afd1619826ba0eb57a9
Collins <ckigen.k@gmail.com>
2022-06-17 14:53:24 +0300

    Create route

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express=require('express')
 const app=express()
 const connectDB=require('./db/connect')
 require('dotenv').config()
+const auth = require('./route/auth')
+
+app.use('/auth', auth)
 
 const PORT=5001
 
